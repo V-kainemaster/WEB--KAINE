fa70817090c6c7d5a226b7ab6321d281268f06c6
V-kainemaster <wenurairaj39145a@gmail.com>
2025-01-14 04:33:46 +0000

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "wenurairaj39145a@gmail.com",
+  password: "j9EeAj5HJZ(SJp7",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
